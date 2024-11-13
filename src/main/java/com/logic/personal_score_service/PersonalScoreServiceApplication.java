02bec2e2f79d990e7e35c94115b4b5c97542e0da
Isaac Neves Farias <isaac.farias@bb.com.br>
2024-11-13 18:19:45 -0300

    primeiro commit

---
+++ src/main/java/com/logic/personal_score_service/PersonalScoreServiceApplication.java
@@ -0,0 +1,13 @@
+package com.logic.personal_score_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalScoreServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalScoreServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/logic/personal_score_service/PersonalScoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logic.personal_score_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalScoreServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
